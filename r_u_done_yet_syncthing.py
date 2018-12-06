19e0a6e05568e13eb56198f386c078226f75c715
Simon Lundström <github-commits@soy.se>
2018-12-06 06:18:12 +0000

    Add basic script

---
+++ r_u_done_yet_syncthing.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#https://forum.syncthing.net/t/how-can-i-check-a-progress-of-a-synchronization-on-a-given-host-from-a-server-side-syncthing-cli/8705
+#https://docs.syncthing.net/rest/stats-device-get.html
+#https://docs.syncthing.net/rest/db-completion-get.html
+
+def main():
+    print("Are you done yet Syncthing?")
+
+if __name__ == "__main__":
+    main()
