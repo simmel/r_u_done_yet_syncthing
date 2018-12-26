baed8c2c18ba633baec3fef2765f4f4d6e28b9d3
Simon Lundström <github-commits@soy.se>
2018-12-26 12:31:41 +0000

    Rename and fix test_check_db_completion_fail

--- r_u_done_yet_syncthing.py
+++ r_u_done_yet_syncthing.py
@@ -2,8 +2,14 @@
 #https://forum.syncthing.net/t/how-can-i-check-a-progress-of-a-synchronization-on-a-given-host-from-a-server-side-syncthing-cli/8705
 #https://docs.syncthing.net/rest/stats-device-get.html
 #https://docs.syncthing.net/rest/db-completion-get.html
+import requests
+from requests.exceptions import ConnectionError
 
 def check_db_completion():
+    try:
+        requests.get('http://localhost:8384/rest/db/completion')
+    except ConnectionError:
+        return False
     return True
 
 def main():
--- test_r_u_done_yet_syncthing.py
+++ test_r_u_done_yet_syncthing.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
-import responses
+import pytest
 import requests
+import responses
 from r_u_done_yet_syncthing import check_db_completion
 
+url_db_completion = 'http://localhost:8384/rest/db/completion'
+
+@responses.activate
 def test_check_db_completion():
+    responses.add(responses.GET, url_db_completion)
     assert check_db_completion() == True
 
-def test_check_db_completion_fail():
-    assert check_db_completion() == False
+@responses.activate
+def test_check_db_completion_api_down():
+    responses.add(responses.GET, url_db_completion,
+                  body=ConnectionError('...'))
+    # https://github.com/getsentry/responses/issues/72
+    # Sadly all exceptions are swallowed so we can't see that it returns False
+    with pytest.raises(ConnectionError):
+        check_db_completion()
