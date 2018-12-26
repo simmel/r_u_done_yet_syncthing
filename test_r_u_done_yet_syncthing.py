3ace10e8b9316a8e91fd33ba021fa1f672838351
Simon Lundström <github-commits@soy.se>
2018-12-26 02:36:14 +0000

    Add test for check_db_completion

---
+++ test_r_u_done_yet_syncthing.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import responses
+import requests
+from r_u_done_yet_syncthing import check_db_completion
+
+def test_check_db_completion():
+    assert check_db_completion() == True
