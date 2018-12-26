ec63506f0e1f8aa1bdc45e36aadbd3e44d6d757c
Simon Lundström <github-commits@soy.se>
2018-12-26 08:17:16 +0000

    Add a failure test

--- test_r_u_done_yet_syncthing.py
+++ test_r_u_done_yet_syncthing.py
@@ -5,3 +5,6 @@ from r_u_done_yet_syncthing import check_db_completion
 
 def test_check_db_completion():
     assert check_db_completion() == True
+
+def test_check_db_completion_fail():
+    assert check_db_completion() == False
