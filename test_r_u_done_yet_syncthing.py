771287eb7b5e51d5c97ba8c0a433036c5bceddf7
Simon Lundström <github-commits@soy.se>
2019-09-06 04:48:59 +0000

    Add deviceID and folder to check DB tests

--- test_r_u_done_yet_syncthing.py
+++ test_r_u_done_yet_syncthing.py
@@ -4,28 +4,33 @@ import pytest
 import requests
 import responses
 import r_u_done_yet_syncthing as rudys
+from urllib.parse import urlencode
 
 url_db_completion = 'http://localhost:8384/rest/db/completion'
+db_completion_params = {
+        'deviceID': 'JGWHCJ5-TBCPDOH-VYSIO3N-GPAQS6Y-YTS5Q5N-NJCDC2U-4SIMPFQ-TFJFQAS',
+        'folder': 'Documents',
+        }
 
 @responses.activate
-def test_check_db_completion():
-    responses.add(responses.GET, url_db_completion)
-    assert rudys.check_db_completion() == True
+def test_check_db_completiona():
+    responses.add(responses.GET, '{}?{}'.format(url_db_completion, urlencode(db_completion_params)))
+    assert rudys.check_db_completion(deviceID=db_completion_params['deviceID'], folder=db_completion_params['folder']) == True
 
 @responses.activate
 def test_check_db_completion_api_down():
-    responses.add(responses.GET, url_db_completion,
+    responses.add(responses.GET, '{}?{}'.format(url_db_completion, urlencode(db_completion_params)),
                   body=ConnectionError('...'))
     # https://github.com/getsentry/responses/issues/72
     # Sadly all exceptions are swallowed so we can't see that it returns False
     with pytest.raises(ConnectionError):
-        rudys.check_db_completion()
+        rudys.check_db_completion(deviceID=db_completion_params['deviceID'], folder=db_completion_params['folder'])
 
 @responses.activate
 def test_check_db_completion_uses_auth(mocker):
     mocker.patch('r_u_done_yet_syncthing.API_KEY', '1337')
-    responses.add(responses.GET, url_db_completion)
-    assert rudys.check_db_completion() == True
+    responses.add(responses.GET, '{}?{}'.format(url_db_completion, urlencode(db_completion_params)),)
+    assert rudys.check_db_completion(deviceID=db_completion_params['deviceID'], folder=db_completion_params['folder']) == True
     assert responses.calls[0].request.headers['X-API-Key'] == '1337'
 
 @responses.activate
